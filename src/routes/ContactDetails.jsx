4007d7765afaa714f100b88448ace29ad20c107c
Caiokl3i <caio.victor.santos12@gmail.com>
2025-10-29 17:20:25 -0400

    feat: add redirect way to navigate between pages

--- src/routes/ContactDetails.jsx
+++ src/routes/ContactDetails.jsx
@@ -1,13 +1,21 @@
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 // 5. Nested routes - useParams para usar identificador após a /
 
 const ContactDetails = () => {
     const { id } = useParams()
 
+    //  6. Redirect
+    const navigate = useNavigate()
+
+    const handleContact = () => {
+        console.log('Contato enviado')
+        return navigate('/')
+    }
+
     return (
         <div>
-            <div>ContactDetails</div>
             <h2>Exibindo informações do contato: {id}</h2>
+            <button onClick={handleContact}>Voltar para home</button>
         </div>
         
     )
