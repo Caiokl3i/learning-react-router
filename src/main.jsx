1f2e3ca2e30309b4161344d37d5343229fcf5666
Caiokl3i <caio.victor.santos12@gmail.com>
2025-10-29 11:12:04 -0400

    feat: configure router setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,28 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// 1. Configurando router
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './routes/Home.jsx'
+import Contact from './routes/Contact.jsx'
+
+// Estrutura de objetos do router
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: 'contact',
+    element: <Contact />
+  }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* Router como props do RouterProvider */}
+    <RouterProvider router={router} />
   </StrictMode>,
 )
